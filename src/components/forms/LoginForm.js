d9ba51b00a1706bcbd6706e806f122d91e2a5f1e
Haley Townsend <haleyrk@hotmail.com>
2018-03-28 17:57:24 -0500

    Making async request to server and render server validation errors

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Form, Button } from 'semantic-ui-react';
-import Validator from 'validator';
-import InlineError from '../messages/InlineError';
+import React from "react";
+import PropTypes from "prop-types";
+import { Form, Button, Message } from "semantic-ui-react";
+import Validator from "validator";
+import InlineError from "../messages/InlineError";
 
 class LoginForm extends React.Component {
   state = {
     data: {
-      email: '',
-      password: ''
+      email: "",
+      password: ""
     },
     loading: false,
     errors: {}
@@ -17,14 +17,18 @@ class LoginForm extends React.Component {
   onChange = e =>
     this.setState({
       data: { ...this.state.data, [e.target.name]: e.target.value }
-  });
+    });
 
   onSubmit = () => {
     const errors = this.validate(this.state.data);
     this.setState({ errors });
     if (Object.keys(errors).length === 0) {
       this.setState({ loading: true });
-      this.props.submit(this.state.data);
+      this.props
+        .submit(this.state.data)
+        .catch(err =>
+          this.setState({ errors: err.response.data.errors, loading: false })
+        );
     }
   };
 
@@ -36,10 +40,16 @@ class LoginForm extends React.Component {
   };
 
   render() {
-    const { data, errors } = this.state;
+    const { data, errors, loading } = this.state;
 
     return (
-      <Form onSubmit={this.Submit}>
+      <Form onSubmit={this.onSubmit} loading={loading}>
+        {errors.global && (
+          <Message negative>
+            <Message.Header>Something went wrong</Message.Header>
+            <p>{errors.global}</p>
+          </Message>
+        )}
         <Form.Field error={!!errors.email}>
           <label htmlFor="email">Email</label>
           <input
